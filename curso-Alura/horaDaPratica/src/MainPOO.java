fd2743bc35d9626f3f083579bc8d12e40515052d
Joao Oliveira <oliveirapedro.joao2301@gmail.com>
2026-01-07 11:47:33 -0300

    practicing implements

--- curso-Alura/horaDaPratica/src/MainPOO.java
+++ curso-Alura/horaDaPratica/src/MainPOO.java
@@ -7,26 +7,26 @@ public class MainPOO {
     static void main() {
         Scanner sc = new Scanner(System.in);
 
-//        ConversorDeMoeda converterDolar = new ConversorDeMoeda();
-//
-//        System.out.println("Digite o valor em dolar que deseja converter: ");
-//        double valorEmDolar = sc.nextDouble();
-//        converterDolar.setValorEmDolar(valorEmDolar);
-//
-//        System.out.println(String.format("%.2fU$ é igual a: %.2fR$", valorEmDolar, converterDolar.converterDolarParaReal()));
+        ConversorDeMoeda converterDolar = new ConversorDeMoeda();
 
-//        CalculadoraSalaRetangular calculadora = new CalculadoraSalaRetangular();
-//
-//        System.out.println("Digite o valor da base: ");
-//        double valorBase = sc.nextDouble();
-//        calculadora.setBase(valorBase);
-//
-//        System.out.println("Digite o valor da altura: ");
-//        double valorAltura = sc.nextDouble();
-//        calculadora.setAltura(valorAltura);
-//
-//        System.out.println("Área do retângulo: " + calculadora.calcularArea());
-//        System.out.println("Perímetro do retângulo: " + calculadora.calcularPerimetro());
+        System.out.println("Digite o valor em dolar que deseja converter: ");
+        double valorEmDolar = sc.nextDouble();
+        converterDolar.setValorEmDolar(valorEmDolar);
+
+        System.out.println(String.format("%.2fU$ é igual a: %.2fR$", valorEmDolar, converterDolar.converterDolarParaReal()));
+
+        CalculadoraSalaRetangular calculadora = new CalculadoraSalaRetangular();
+
+        System.out.println("Digite o valor da base: ");
+        double valorBase = sc.nextDouble();
+        calculadora.setBase(valorBase);
+
+        System.out.println("Digite o valor da altura: ");
+        double valorAltura = sc.nextDouble();
+        calculadora.setAltura(valorAltura);
+
+        System.out.println("Área do retângulo: " + calculadora.calcularArea());
+        System.out.println("Perímetro do retângulo: " + calculadora.calcularPerimetro());
 
 
     }
