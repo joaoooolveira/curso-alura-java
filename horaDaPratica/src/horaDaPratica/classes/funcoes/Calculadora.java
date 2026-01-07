2084bbb18ad4b5f58d5630390aaaeae6532a21e0
Joao Oliveira <oliveirapedro.joao2301@gmail.com>
2026-01-07 11:46:36 -0300

    practicing implements

---
+++ horaDaPratica/src/horaDaPratica/classes/funcoes/Calculadora.java
@@ -0,0 +1,13 @@
+package horaDaPratica.classes.funcoes;
+
+public class Calculadora {
+    private int numero;
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public int dobroDoNumero(){
+        return numero *= 2;
+    }
+}
---
+++ horaDaPratica/src/horaDaPratica/classes/funcoesPOO/ConversaoFinanceira.java
@@ -0,0 +1,5 @@
+package horaDaPratica.classes.funcoesPOO;
+
+public interface ConversaoFinanceira {
+    double converterDolarParaReal();
+}
