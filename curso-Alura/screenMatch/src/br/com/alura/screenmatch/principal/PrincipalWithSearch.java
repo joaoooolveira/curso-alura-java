c3f95764e16e1a4bb444a94ed73d074e2e4a2bab
Joao Oliveira <oliveirapedro.joao2301@gmail.com>
2026-01-10 15:15:21 -0300

    adding search with API

--- curso-Alura/screenMatch/src/br/com/alura/screenmatch/principal/PrincipalWithSearch.java
+++ curso-Alura/screenMatch/src/br/com/alura/screenmatch/principal/PrincipalWithSearch.java
@@ -5,12 +5,19 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Scanner;
 
 public class PrincipalWithSearch {
     static void main() throws IOException, InterruptedException {
+        Scanner leitura = new Scanner(System.in);
+        System.out.print("Digite um filme que deseja buscar: ");
+        String buscaDeFilme = leitura.nextLine();
+
+        String endereco = "https://www.omdbapi.com/?t=" + buscaDeFilme + "&apikey=90aa8218";
+
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://www.omdbapi.com/?t=matrix&apikey=90aa8218"))
+                .uri(URI.create(endereco))
                 .build();
         HttpResponse<String> response = client
                 .send(request, HttpResponse.BodyHandlers.ofString());
