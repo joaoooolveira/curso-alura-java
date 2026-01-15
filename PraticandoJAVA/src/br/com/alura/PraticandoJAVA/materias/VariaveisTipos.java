dad8145f41630bf8d0dce411c7197df24b922796
Joao Oliveira <oliveirapedro.joao2301@gmail.com>
2026-01-15 09:01:09 -0300

    exercise 4 - temperature convert

--- PraticandoJAVA/src/br/com/alura/PraticandoJAVA/materias/VariaveisTipos.java
+++ PraticandoJAVA/src/br/com/alura/PraticandoJAVA/materias/VariaveisTipos.java
@@ -1,12 +1,17 @@
 package br.com.alura.PraticandoJAVA.materias;
 
+import br.com.alura.PraticandoJAVA.metodos.ConversaoDeTemperatura;
 import br.com.alura.PraticandoJAVA.metodos.Estudante;
 import br.com.alura.PraticandoJAVA.metodos.Notas;
 
+import java.util.Scanner;
+
 public class VariaveisTipos {
     public static void main(String[] args) {
+        Scanner leitura = new Scanner(System.in);
+
         //Escreva um programa que declare variáveis para o seu nome, idade, altura e se você é estudante,
-        // utilizando os tipos apropriados, e depois exiba os valores armazenados.
+        //utilizando os tipos apropriados, e depois exiba os valores armazenados.
 
         System.out.println("Exercício 1:");
         Estudante maria = new Estudante("Maria", 25, 1.68, true);
@@ -14,8 +19,8 @@ public class VariaveisTipos {
         System.out.println(maria + "\n");
 
         //Você está desenvolvendo um sistema de gerenciamento de estoque que exige que as quantidades dos produtos sejam registradas como números inteiros.
-        // Isso ocorre porque um estoque físico não pode conter frações de um item, por exemplo, não faz sentido ter "19.5" unidades de um produto.
-        // No entanto, os dados disponíveis atualmente estão no formato double, que permite valores decimais.
+        //Isso ocorre porque um estoque físico não pode conter frações de um item, por exemplo, não faz sentido ter "19.5" unidades de um produto.
+        //No entanto, os dados disponíveis atualmente estão no formato double, que permite valores decimais.
         //
         //Para atender às exigências do sistema, crie um programa que converta a quantidade de double para int, garantindo que os valores sejam adequados ao formato aceito.
         System.out.println("Exercício 2:");
@@ -24,12 +29,26 @@ public class VariaveisTipos {
         System.out.println((int) valorEntrada + "\n");
 
         //Você está desenvolvendo um aplicativo que calcula a média de notas dos alunos. Para isso, você precisa armazenar as notas em variáveis do tipo double.
-        // Você decide criar três variáveis chamadas nota1, nota2 e nota3, e atribuir a elas os valores 7.5, 8.0 e 9.0, respectivamente e em seguida,
-        // calcular a média e exibi-la na tela.
+        //Você decide criar três variáveis chamadas nota1, nota2 e nota3, e atribuir a elas os valores 7.5, 8.0 e 9.0, respectivamente e em seguida,
+        //calcular a média e exibi-la na tela.
         System.out.println("Exercício 3:");
         Notas notas = new Notas(7.5, 8.0, 9.0);
 
         notas.calcularMedia(3);
-        System.out.println(notas);
+        System.out.println(notas + "\n");
+
+        //Você está desenvolvendo um sistema de monitoramento de temperatura para uma estufa de plantas.
+        //Você recebe os dados de temperatura em graus Celsius, mas precisa exibi-los em Fahrenheit
+        //para um relatório que será enviado para um cliente nos Estados Unidos.
+        //
+        //Crie um programa que receba uma temperatura em graus Celsius (int) e exiba a temperatura convertida
+        //em graus Fahrenheit (double).
+        System.out.println("Exercício 4:");
+        System.out.print("Digite a temperatura atual: ");
+        double celsius = leitura.nextDouble();
+
+        ConversaoDeTemperatura converter = new ConversaoDeTemperatura();
+        converter.ConverterTemperaturaParaFahrenheit(celsius);
+        System.out.println(converter);
     }
 }
