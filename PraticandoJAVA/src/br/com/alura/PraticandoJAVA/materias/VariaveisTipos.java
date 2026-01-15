16e698b9967e429230a9f33dc5e6728597ec804c
Joao Oliveira <oliveirapedro.joao2301@gmail.com>
2026-01-15 08:35:50 -0300

    first exercise - declaring values to variables

---
+++ PraticandoJAVA/src/br/com/alura/PraticandoJAVA/materias/VariaveisTipos.java
@@ -0,0 +1,11 @@
+package br.com.alura.PraticandoJAVA.materias;
+
+import br.com.alura.PraticandoJAVA.metodos.Estudante;
+
+public class VariaveisTipos {
+    public static void main(String[] args) {
+        Estudante maria = new Estudante("Maria", 25, 1.68, true);
+
+        System.out.println(maria);
+    }
+}
