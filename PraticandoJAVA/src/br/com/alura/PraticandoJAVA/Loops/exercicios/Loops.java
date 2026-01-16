ba630387183ab7fad64bf21123bf60b8c7e4cd3b
Joao Oliveira <oliveirapedro.joao2301@gmail.com>
2026-01-16 10:27:19 -0300

    loops - sum of the even numbers between 1-100

--- PraticandoJAVA/src/br/com/alura/PraticandoJAVA/Loops/exercicios/Loops.java
+++ PraticandoJAVA/src/br/com/alura/PraticandoJAVA/Loops/exercicios/Loops.java
@@ -33,5 +33,18 @@ public class Loops {
              soma += valores[i];
         }
         System.out.println(soma + "\n");
+
+        //Maria quer calcular a soma de todos os números pares de 1 a 100.
+        //Ela decidiu usar um loop para iterar pelos números e somar apenas aqueles divisíveis por 2.
+        //Crie um programa que realize essa tarefa.
+        int somaPares = 0;
+
+        for (int i = 0; i <= 100; i++) {
+            if(i % 2 == 0){
+                somaPares += i;
+
+            }
+        }
+        System.out.println(somaPares);
     }
 }
