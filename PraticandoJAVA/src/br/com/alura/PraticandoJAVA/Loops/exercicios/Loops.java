5e3d7b6d1d3dc470b96e29d703df9e2818d92653
Joao Oliveira <oliveirapedro.joao2301@gmail.com>
2026-01-16 11:22:53 -0300

    loops - system of password
    this system ask for the password to you, if the password's wrong, askes again, when you reach 3 times, it stops

--- PraticandoJAVA/src/br/com/alura/PraticandoJAVA/Loops/exercicios/Loops.java
+++ PraticandoJAVA/src/br/com/alura/PraticandoJAVA/Loops/exercicios/Loops.java
@@ -76,6 +76,31 @@ public class Loops {
             }
         }
 
-        System.out.println("O maior número é: " + maiorNumero);
+        System.out.println("O maior número é: " + maiorNumero + "\n");
+
+        //Leonardo trabalha no setor de segurança de um sistema bancário e precisa garantir que os usuários tenham um número
+        // limitado de tentativas para inserir a senha corretamente. Se o usuário errar a senha 3 vezes seguidas, sua conta deve ser bloqueada temporariamente.
+        //
+        //Com base nesse cenário, crie um programa que peça uma senha ao usuário e permita até 3 tentativas de erro antes de bloquear o acesso.
+        // Se o usuário digitar corretamente antes disso, o sistema deve conceder o acesso imediatamente.
+        int tentativas = 0;
+        String senhaCorreta = "1234";
+
+        while(tentativas <= 2){
+            System.out.print("Digite a senha: ");
+            String senha = leitura.next();
+
+            if(senhaCorreta.equals(senha)){
+                System.out.println("Acesso liberado!");
+                break;
+            } else {
+                System.out.println("Senha incorreta.");
+                tentativas++;
+            }
+        }
+
+        if(tentativas == 3){
+            System.out.println("Conta bloqueada temporariamente.");
+        }
     }
 }
