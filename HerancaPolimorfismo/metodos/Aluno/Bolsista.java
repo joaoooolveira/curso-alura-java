b3435ae54983a9c50279032a31a48b12a8688fb2
Joao Oliveira <oliveirapedro.joao2301@gmail.com>
2026-01-21 15:32:17 -0300

    Delete HerancaPolimorfismo directory duplicate

--- HerancaPolimorfismo/metodos/Aluno/Bolsista.java
+++
@@ -1,7 +0,0 @@
-package br.com.alura.PraticandoJAVA.HerancaPolimorfismo.metodos.Aluno;
-
-public class Bolsista extends Aluno {
-    public Bolsista(String nomeAluno) {
-        super(nomeAluno, "Bolsista");
-    }
-}
