43e62188b3c74bb9675fcfd9090b0916978b666e
Joao Oliveira <oliveirapedro.joao2301@gmail.com>
2026-02-02 11:43:59 -0300

    Strings - remove backspaces

---
+++ PraticandoJAVA/src/br/com/alura/PraticandoJAVA/Strings/exercicios/Strings.java
@@ -0,0 +1,21 @@
+package br.com.alura.PraticandoJAVA.Strings.exercicios;
+
+import java.util.Scanner;
+
+public class Strings {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        //Joana está desenvolvendo um sistema de cadastro de usuários e percebeu que muitos nomes estão sendo cadastrados com espaços
+        // em branco no início ou no final. Ela quer um programa que remova esses espaços automaticamente.
+        //
+        //Crie um programa que receba um nome e exiba o nome sem espaços em branco no início ou no final.
+        System.out.print("Digite seu nome: ");
+        String nome = sc.nextLine();
+
+        String nomeFormatado = nome.trim();
+        System.out.println("Nome sem espaços: " + nomeFormatado + "\n");
+
+        
+    }
+}
